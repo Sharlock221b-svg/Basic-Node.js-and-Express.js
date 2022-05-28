6b2a7d4b61ff0c3aff6b981fb17bf742839bef21
Dhairya Upadhyay <dhairya221b@gmail.com>
2022-05-28 19:28:39 +0530

    Serve the JSON and Usa the .env file

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 console.log("Hello World");
 
 app.use("/public", express.static(__dirname + "/public"));
@@ -8,7 +9,17 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/views/index.html");
 })
 
-
+app.get("/json", function(req, res){
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        response = "Hello json".toUpperCase();
+      } else {
+        response = "Hello json";
+      }
+      
+    res.json({
+        "message": response
+    });
+})
 
 
 
